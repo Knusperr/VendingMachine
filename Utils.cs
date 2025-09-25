f9ff1135a31a54c1c8532fbb446ea4a91ac1629b
Shio <L.setzen@gmx.de>
2025-09-25 14:15:22 +0200

    Create Utils.cs

---
+++ Utils.cs
@@ -0,0 +1,18 @@
+﻿namespace VendingMachine
+{
+    /* 
+     * Schreibt ein Programm, welches einen Getränkeautomaten nachbildet:
+     * Der Spieler beginnt mit einem festen Geldbetrag und darf diesen am Automaten ausgeben.
+     * Die Auswahl der Getränke soll über Nutzereingabe passieren.
+     * Wenn ein Getränk und der entsprechende Geldbetrag eingegeben wurde, soll der Spieler ein Getränk in sein Inventar
+     * übergeben bekommen. Dieses bekommt er nach jedem Kauf aufgelistet.
+     * 
+     * Je nach Stand, kann man weitere Features zum Automaten hinzufügen.
+     * Getränk trinken können... Füllstatus des Automaten... Text-Effekte und Farbe...
+     */
+
+    internal class Utils
+    {
+
+    }
+}
