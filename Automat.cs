295f9e9356ba5caa0a0edc1bb483fd27c3e81fbd
Student <student@sae.de>
2025-09-25 14:29:01 +0200

    added: Automatenmethoden

---
+++ Automat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class Automat
+    {
+        public void DisplayDrinks()
+        {
+        }
+        public void SelectDrinks()
+        {
+
+        }
+        public void Buy()
+        {
+
+        }
+        public void InputMoney(float value)
+        {
+
+        }
+
+
+    }
+}
